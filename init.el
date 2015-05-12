5947fdf86692d99ca0bfb81f8efb234849656180
Akbar <ed.akbar@gmail.com>
2015-05-12 12:32:25 +0530

    Update init.el
    May 2015 - Fedora 21, emacs config

--- init.el
+++ init.el
@@ -18,12 +18,24 @@
 
 '(initial-buffer-choice scratch)
 
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- ))
+
+'(python-shell-interpreter "python3")
+
+
+;; autloads
+(if (locate-library "ediff-trees")
+    (autoload 'ediff-trees "ediff-trees" "Start an tree ediff" t))
+
+;; autcompile
+(defun autocompile nil
+  "compile itself if ~/.emacs.d/init.el"
+  (interactive)
+  (require 'bytecomp)
+  (let ((dotemacs (expand-file-name "~/.emacs.d/init.el")))
+    (if (string= (buffer-file-name) (file-chase-links dotemacs))
+      (byte-compile-file dotemacs))))
+
+(add-hook 'after-save-hook 'autocompile)
 
 ;; C-x C-j opens dired with the cursor right on the file you're editing
 (require 'dired-x)
@@ -36,6 +48,14 @@
 (global-set-key [f11] 'fullscreen)
 
 ;; Load File List
+(add-to-list 'custom-theme-load-path "~/.emacs.d/monokai-emacs")
+
+;; paren mode
+
+(setq show-paren-delay 0)           ; how long to wait?
+(show-paren-mode t)                 ; turn paren-mode on
+(setq show-paren-style 'expression) ; alternatives are 'parenthesis' and 'mixed'
+
 
 ;; hide keys
 (add-hook 'comint-output-filter-functions
@@ -48,8 +68,6 @@
 ;; ESS
 (load "~/.emacs.d/ESS/lisp/ess-site")
 
-;;ipython
-;;(add-to-list 'load-path "c:/Users/mohammed.akbar/Document/python--3.3.3")
 
 ;;Toolbar and menu bar distract you from learning the key
 ;;bindings (i.e. becoming efficient).
@@ -57,9 +75,6 @@
 (menu-bar-mode 0)
 (scroll-bar-mode 0)
 
-;;Font pickup
-(set-face-attribute 'default nil
-                    :family "Consolas" :height 100)
 
 ;; ==================================================
 ;;  org mode
@@ -82,18 +97,6 @@
     (setq ido-enable-flex-matching t) ;; enable fuzzy matching
 
 
-;; ==================================================
-;;  AutoComplete
-;; ==================================================
-
-;(require 'auto-complete-config)
-
-;add-to-list 'ac-dictionary-directories "~/.emacs.d/elpa/auto-complete-1.4.0/ac-dict")
-;(ac-config-default)
-
-;(setq ess-use-auto-complete 'script-only)
-;(define-key ac-completing-map (kbd "M-h") 'ac-quick-help)
-
 
 ;; ==================================================
 ;;  EMACS Speaks Statistics
@@ -104,17 +107,16 @@
  (setq use-file-dialog nil)
  (setq ess-eval-visibly-p nil) ;otherwise C-c C-r (eval region) takes forever
  (setq ess-ask-for-ess-directory nil) ;otherwise you are prompted each time you start an interactive R session
- ;;(setq inferior-R-program-name "C:/Users/mohammed.akbar/Document/R/R-3.0.2/bin/x64/Rterm.exe")
 
 ;;; C
-(add-hook 'c-mode-hook
+;(add-hook 'c-mode-hook
           ;;(lambda () (c-set-style "bsd")))
           ;;(lambda () (c-set-style "user"))) ; edd or maybe c++ ?
-          (lambda () (c-set-style "c++"))) ; edd or maybe c++ ?
+;          (lambda () (c-set-style "c++"))) ; edd or maybe c++ ?
 ;;;; ESS
-(add-hook 'ess-mode-hook
-          (lambda ()
-            (ess-set-style 'C++)
+;(add-hook 'ess-mode-hook
+;          (lambda ()
+;            (ess-set-style 'C++)
         ;; Because
             ;;                                 DEF GNU BSD K&R C++
             ;; ess-indent-level                  2   2   8   5   4
@@ -124,15 +126,14 @@
             ;; ess-expression-offset             4   2   8   5   4
             ;; ess-else-offset                   0   0   0   0   0
             ;; ess-close-brace-offset            0   0   0   0   0
-            (add-hook 'local-write-file-hooks
-                      (lambda ()
-                        (ess-nuke-trailing-whitespace)))))
+;            (add-hook 'local-write-file-hooks
+;                      (lambda ()
+;                        (ess-nuke-trailing-whitespace)))))
 (setq ess-nuke-trailing-whitespace-p t)
 
+(require 'ess-site)
+(require 'ess-eldoc)
 
- (require 'ess-site)
- ;(require 'ess-R-object-tooltip)
- (require 'ess-eldoc)
 
 ;; ==================================================
 ;;  Marmalade
@@ -149,52 +150,46 @@
 ;;  python
 ;; ==================================================
 
+
 (require 'python)
 
-;; setup ipython for default interpreter
-;;(setq
-;; python-shell-interpreter "C:\\Users\\mohammed.akbar\\Document\\Python--3.3.3\\python.exe"
-;; python-shell-interpreter-args
-;; "-i C:\\Users\\mohammed.akbar\\Document\\Python--3.3.3\\Scripts\\ipython3-script.py")
-
-;; ipy notebook
-;(require 'ein)
 
 ;; ==================================================
 ;;  el-get
 ;; ==================================================
 
-(add-to-list 'load-path "~/.emacs.d/el-get/el-get")
+;; (add-to-list 'load-path "~/.emacs.d/el-get/el-get")
 
-(unless (require 'el-get nil 'noerror)
-  (with-current-buffer
-      (url-retrieve-synchronously
-       "https://raw.github.com/dimitri/el-get/master/el-get-install.el")
-    (let (el-get-master-branch)
-      (goto-char (point-max))
-      (eval-print-last-sexp))))
+;; (unless (require 'el-get nil 'noerror)
+;;   (with-current-buffer
+;;       (url-retrieve-synchronously
+;;        "https://raw.github.com/dimitri/el-get/master/el-get-install.el")
+;;     (let (el-get-master-branch)
+;;       (goto-char (point-max))
+;;       (eval-print-last-sexp))))
 
-(el-get 'sync) 
+;; (el-get 'sync)
+
+;; load theme
+(load-theme 'monokai t)
 
 ;; ==================================================
-;;  Scala Hooks
+;;  graphene
 ;; ==================================================
 
-;;(add-hook 'scala-mode-hook '(lambda ()
- ;; (require 'whitespace)
+(require 'graphene)
 
-  ;; clean-up whitespace at save
-  ;;(make-local-variable 'before-save-hook)
- ;; (add-hook 'before-save-hook 'whitespace-cleanup)
+;; -----------------------------------------------------------------------------
+;; Git support
+;; -----------------------------------------------------------------------------
 
-  ;; turn on highlight. To configure what is highlighted, customize
-  ;; the *whitespace-style* variable. A sane set of things to
-  ;; highlight is: face, tabs, trailing
-;  (whitespace-mode)
-;))
+(add-to-list 'load-path "/usr/share/doc/git/contrib/emacs")
+(require 'git)
+(require 'git-blame)
 
-(require 'scala-mode2)
-(require 'ensime)
-(require 'sbt-mode)
-
-(add-hook 'scala-mode-hook 'ensime-scala-mode-hook)
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :background "#272822" :foreground "#F8F8F2" :weight light :height 113 :foundry "unknown" :family "M+ 1mn"))))))
